cfbf4f21b57b1c89a3f43d84659f55f9403f0e8e
AngeloEsam <150146644+AngeloEsam@users.noreply.github.com>
2025-07-30 20:05:47 +0300

    Update api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://movie-n7v9k327g-anglos-projects.vercel.app/api',
 });
 
 export default api;
