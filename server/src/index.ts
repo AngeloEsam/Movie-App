e78d5cc1a98fd7d938441c59db7270eed812f852
Angelo Esam <angloesam61@gmail.com>
2025-07-30 20:04:58 +0300

    finish

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mediaRoutes from './routes/media.routes';
 import errorHandler from './middleware/errorHandler';
-
+import createServerlessExpress from '@vendia/serverless-express';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}
+));
 app.use(express.json());
 
 app.use('/api/media', mediaRoutes);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default createServerlessExpress({ app });
